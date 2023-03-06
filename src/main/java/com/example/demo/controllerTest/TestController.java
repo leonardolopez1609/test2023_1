63e3fe44744e8e12cf258abbe5844783402770c7
leonardolopez1609 <leonardojoselc@ufps.edu.co>
2023-03-06 11:06:34 -0500

    Test Controller

--- src/main/java/com/example/demo/controllerTest/TestController.java
+++ src/main/java/com/example/demo/controllerTest/TestController.java
@@ -1,11 +1,19 @@
 package com.example.demo.controllerTest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/test")
 @RestController
 public class TestController {
 
 
 	
+	@GetMapping
+	public String test1() {
+		return "test";
+		
+	}
 	
 }
